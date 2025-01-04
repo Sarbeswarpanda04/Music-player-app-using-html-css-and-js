621399d18682d11ea9b80ea2b9b639582ce441f5
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2025-01-05 02:15:27 +0530

    add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,46 @@
+let allMusic = [
+
+    {
+        name: "Brown Rong",
+        artist: "Yo YO Honey Singh",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "tum hi ho",
+        artist: "Arijit Singh",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Choti Si Pyarisi Nanhisi",
+        artist: "Anari",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "tumsa koi pyara",
+        artist: "Kishore Kumar",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "kaun tujhe",
+        artist: "Amaal Mallik",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Bol Do Na Zara",
+        artist: "Armaan Malik",
+        img: "music-6",
+        src: "music-6"
+    },
+    
+    {
+        name: "jagata re paibuni..",
+        artist: "Satyajeet jena",
+        img: "music-7",
+        src: "music-7"
+    },
+    ];
